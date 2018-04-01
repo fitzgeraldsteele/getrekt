978f4751420784aff55f5e8c99cb2713b15fe096
Jerry Steele <fisteele@microsoft.com>
2018-03-31 18:57:48 -0700

    Add project files.

---
+++ App2/App2/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new App2.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("ios=d0af9a4b-6edc-4e45-bfa6-477ddd0cc08f;" +
+                  //"uwp={Your UWP App secret here};" +
+                  "android=e56b418b-8094-4959-a134-c8b9a9835637",
+                  typeof(Analytics), typeof(Crashes));
+
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+
+        }
+
+        private void MainBtn_OnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("Get Rekt button clicked!");
+                var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+                player.Load("instantrapairhorn.mp3");
+                //player.Load(Path.Combine("Assets","censorbeep4.mp3"));
+
+                // Read the contents of our asset
+                //var assembly = typeof(App).GetTypeInfo().Assembly;
+                //Stream audioStream = assembly.GetManifestResourceStream("Audio/censor_beep_4.mp3");
+
+           
+                //var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+                //player.Load(audioStream);
+
+                player.Play();
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+                Debug.WriteLine("Error playing sound");
+                Debug.WriteLine(exception.ToString());
+                //throw (exception);
+            }
+
+        }
+	}
+}
